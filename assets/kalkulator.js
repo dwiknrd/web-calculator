ab77f8f81114c24cf46859c6dc34798bc2c66019
Dwi Gustin Nurdialit <dwiknrd@gmail.com>
2022-08-10 11:58:14 +0700

    initial commit

---
+++ assets/kalkulator.js
@@ -0,0 +1,106 @@
+const calculator = {
+    displayNumber: '0',
+    operator: null,
+    firstNumber: null,
+    waitingForSecondNumber: false
+};
+
+function updateDisplay() {
+    document.querySelector('#displayNumber').innerText = calculator.displayNumber;
+}
+
+function clearCalculator() {
+    calculator.displayNumber = '0';
+    calculator.operator = null;
+    calculator.firstNumber = null;
+    calculator.waitingForSecondNumber = false;
+    updateDisplay();
+}
+
+function inputDigit(digit) {
+    if (calculator.displayNumber === '0') {
+        calculator.displayNumber = digit;
+    } else {
+        calculator.displayNumber += digit;
+    }
+}
+
+const button = document.querySelectorAll(".btn");
+for (let btn of button) {
+    btn.addEventListener('click', function(event) {
+        const target = event.target;
+
+        if (target.classList.contains('clear')) {
+            clearCalculator();
+            updateDisplay();
+            return;
+        }
+
+        if (target.classList.contains('equals')) {
+            performCalculation();
+            updateDisplay();
+            return;
+        }
+
+        if (target.classList.contains('negative')) {
+            inverseNumber();
+            updateDisplay();
+            return;
+        }
+
+        if (target.classList.contains('operator')) {
+            handleOperator(target.innerText);
+            return;
+        }
+
+        inputDigit(target.innerText);
+        updateDisplay();
+    });
+}
+
+function inverseNumber() {
+    if (calculator.displayNumber === '0') {
+        return;
+    }
+
+    calculator.displayNumber = calculator.displayNumber * -1;
+}
+
+function handleOperator(operator) {
+    if (!calculator.waitingForSecondNumber) {
+        calculator.operator = operator;
+        calculator.waitingForSecondNumber = true;
+        calculator.firstNumber = calculator.displayNumber;
+
+        calculator.displayNumber = '0';
+    } else {
+        alert('Operator tidak boleh ditambahkan lagi');
+    }
+}
+
+function performCalculation() {
+    if (calculator.firstNumber == null || calculator.operator == null) {
+        alert('Operator belum ditambahkan');
+        return;
+    }
+
+    let result = 0;
+    if (calculator.operator === '+') {
+        result = parseInt(calculator.firstNumber) + parseInt(calculator.displayNumber);
+    } else if (calculator.operator === '-') {
+        result = parseInt(calculator.firstNumber) - parseInt(calculator.displayNumber);
+    }
+
+    const history = {
+        firstNumber: calculator.firstNumber,
+        operator: calculator.operator,
+        secondNumber: calculator.displayNumber,
+        result: result
+    }
+
+    putHistory(history);
+    calculator.displayNumber = result;
+    renderHistory();
+
+    calculator.displayNumber = result.toString();
+}
